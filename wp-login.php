2de6f0e812ccf1a5d96267c26095b9d318a3f115
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2021-03-01 12:42:37 +0100

    Initial commit

---
+++ wp-login.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Entra &lsaquo; Invisere &#8212; WordPress</title>
+	<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="dashicons-css" href="https://invisere.github.io/wp-includes/css/dashicons.min.css?ver=5.6.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://invisere.github.io/wp-includes/css/buttons.min.css?ver=5.6.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://invisere.github.io/wp-admin/css/forms.min.css?ver=5.6.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://invisere.github.io/wp-admin/css/l10n.min.css?ver=5.6.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://invisere.github.io/wp-admin/css/login.min.css?ver=5.6.2" type="text/css" media="all">
+	<meta name="robots" content="noindex,noarchive">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-ca">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Funcionant amb el WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://invisere.github.io/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nom d'usuari o adreça de correu electrònic</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Contrasenya</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostra la contrasenya">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Recorda&#039;m</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Entra">
+									<input type="hidden" name="redirect_to" value="https://invisere.github.io/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="https://invisere.github.io/wp-login.php?action=lostpassword">Heu perdut la contrasenya?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="https://invisere.github.io/">
+		&larr; Vés a Invisere		</a></p>
+			</div>
+	<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/invisere.github.io\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script type="text/javascript" id="wp-polyfill-js-after">
+( 'fetch' in window ) || document.write( '<script src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://invisere.github.io/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );
+</script>
+<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/dist/i18n.min.js?ver=ac389435e7fd4ded01cf603f3aaba6a6" id="wp-i18n-js"></script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Potència desconeguda de la contrasenya","short":"Molt feble","bad":"Feble","good":"Mitjana","strong":"Forta","mismatch":"No coincideix"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2021-01-30 07:47:39+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"ca"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["La funció %1$s és obsoleta des de la versió %2$s! Useu %3$s per contra. Considereu l'escriptura de codi més inclusiu."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+</script>
+<script type="text/javascript" src="https://invisere.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.6.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/invisere\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://invisere.github.io/wp-includes/js/wp-util.min.js?ver=5.6.2" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2021-01-30 07:47:39+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"ca"},"Your new password has not been saved.":["La contrasenya nova no s'ha desat."],"Hide":["Amaga"],"Show":["Mostra"],"Show password":["Mostra la contrasenya"],"Confirm use of weak password":["Confirmeu l'ús de contrasenya feble"],"Hide password":["Amaga la contrasenya"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+</script>
+<script type="text/javascript" src="https://invisere.github.io/wp-admin/js/user-profile.min.js?ver=5.6.2" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
